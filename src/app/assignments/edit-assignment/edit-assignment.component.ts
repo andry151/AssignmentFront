3858d7f29f668130cc974123e033a208b470f3ba
Andry Razafindrazaka <andry.razafind@gmail.com>
2022-04-26 21:30:55 +0200

    Update fonctionnel

--- src/app/assignments/edit-assignment/edit-assignment.component.ts
+++ src/app/assignments/edit-assignment/edit-assignment.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AssignmentsService } from 'src/app/shared/assignments.service';
 import { Assignment } from '../model/assignment.model';
 import {AuthService} from "../../shared/auth.service";
+import {Matiere} from "../model/matiere.model";
 
 @Component({
   selector: 'app-edit-assignment',
@@ -13,6 +14,11 @@ export class EditAssignmentComponent implements OnInit {
   assignment!: Assignment | undefined;
   nomAssignment!: string;
   dateDeRendu!: Date;
+  auteur!: string;
+  remarques!: string;
+  note!: number;
+  matiere!: Matiere;
+  matieres!:Matiere[];
 
   constructor(
     private assignmentsService: AssignmentsService,
@@ -22,13 +28,13 @@ export class EditAssignmentComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
+    this.matieres = this.assignmentsService.matieres;
     // ici un exemple de récupération des query params et du fragment
-    let queryParams = this.route.snapshot.queryParams;
+    /*let queryParams = this.route.snapshot.queryParams;
     console.log("Query params :")
     console.log(queryParams);
     console.log("Fragment :")
-    console.log(this.route.snapshot.fragment);
+    console.log(this.route.snapshot.fragment);*/
 
     this.getAssignment();
   }
@@ -46,6 +52,10 @@ export class EditAssignmentComponent implements OnInit {
       // Pour pré-remplir le formulaire
       this.nomAssignment = assignment.nom;
       this.dateDeRendu = assignment.dateDeRendu;
+      this.auteur = assignment.auteur;
+      if (assignment.note) this.note = assignment.note;
+      if (assignment.remarques) this.remarques = assignment.remarques;
+      this.matiere = assignment.matiere;
     });
   }
 
@@ -55,6 +65,18 @@ export class EditAssignmentComponent implements OnInit {
     // on récupère les valeurs dans le formulaire
     this.assignment.nom = this.nomAssignment;
     this.assignment.dateDeRendu = this.dateDeRendu;
+    this.assignment.auteur = this.auteur;
+    if (this.note)
+    {
+      if (this.assignment.rendu === false) {
+        this.assignment.rendu = true;
+        console.log("mande le true");
+      }
+      this.assignment.note = this.note;
+    }
+    if (this.remarques) this.assignment.remarques = this.remarques;
+    this.assignment.matiere = this.matiere;
+
 
     this.assignmentsService
       .updateAssignment(this.assignment)
